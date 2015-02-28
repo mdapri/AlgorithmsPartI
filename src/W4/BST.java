1055bf8aba03d4e8c454557f0d1532d1bf79d99d
mdapri <maurizio.dapri@gmail.com>
2015-02-28 19:52:01 +0100

    added the delete methods and counts

--- src/W4/BST.java
+++ src/W4/BST.java
@@ -15,6 +15,7 @@ public class BST<Key extends Comparable<Key>, Value>
         private Key key;
         private Value val;
         private Node left, right;
+        private int count;
 
         public Node(Key key, Value val) {
             this.key = key;
@@ -26,6 +27,14 @@ public class BST<Key extends Comparable<Key>, Value>
         public String toString(){
             return String.format("%s / %s",key,val);
         }
+    }
+
+    public int size()
+    { return size(root); }
+    private int size(Node x)
+    {
+        if (x == null) return 0;
+        return x.count;
     }
 
     //    put key-value pair into the table
@@ -48,12 +57,22 @@ public class BST<Key extends Comparable<Key>, Value>
         if (cmp == 0) {
             x.val = val;
         }
+        x.count = 1 + size(x.left) + size(x.right);
         return x;
     }
 
     public Value get(Key key)
     {
-        Node x=root;
+        Node x=get(root,key);
+        if (x != null)
+            return x.val;
+        else
+
+            return null;
+    }
+
+    private Node get(Node x,Key key){
+
         while(x!=null) {
             int cmp = key.compareTo(x.key);
             if (cmp >0)
@@ -61,18 +80,59 @@ public class BST<Key extends Comparable<Key>, Value>
             else if (cmp <0)
                 x = x.left;
             if (cmp == 0) {
-                return x.val ;
+                return x ;
             }
         }
         return null;
     }
 
-
     //not lazy. The lazy implementation requires a restructuring of the tree
+    // this will be the Hibbard deletion
     public void delete(Key key)
     {
+        //node to eliminate
+        root=delete(root,key);
+    }
+
+    // deletes the key starting the search from the node x
+    // and returns the value to be put as the parent new descendnt (new root of the subtree)
+    private Node delete(Node x, Key key){
+        if(x==null) return null;
+        int cmp = key.compareTo(x.key);
+        if (cmp>0) x.right=delete(x.right,key);
+        else if (cmp<0) x.left=delete(x.left,key);
+        else { //node found
+            //delete node has one child
+            if (x.left == null)
+                x = x.right;
+            else if (x.right == null)
+                x = x.left;
+                //deleted node has 2 children
+            else {
+                Node t = x;
+                //get the successor
+                x=min(t.right);
+                //adjust the successor and subtree of the new root
+                x.right=deleteMin(t.right);
+                x.left=t.left;
+            }
+        }
+        x.count = 1 + size(x.left) + size(x.right);
+        return x;
+    }
+
+    private Node deleteMin(Node x) {
+        if(x== null) return null;
+        if(x.left!=null)
+            x.left= deleteMin(x.left);
+        else
+            return x.right;
+        x.count = 1 + size(x.left) + size(x.right);
+        return x;
 
     }
+
+
     public Iterable<Key> iterator()
     { /* see next slides */ return null;}
 
@@ -88,12 +148,19 @@ public class BST<Key extends Comparable<Key>, Value>
 
 
     public Key min(){
-        Node x=root;
-        while (x.right!=null) {
-            x = x.left;
-        }
+        Node x=min(root);
         return x.key;
     }
+
+    //starting from the node find the minimum
+    private Node min(Node x){
+
+        while (x.left!=null) {
+            x = x.left;
+        }
+        return x;
+    }
+
 
     //returns the largest key &lte; a given key
     public Key floor(Key key) {
@@ -107,7 +174,7 @@ public class BST<Key extends Comparable<Key>, Value>
 
     private Node floor(Node x,Key key) {
         //if the node is null there is no bigger key that is  less that the given key, so return
-        MyUtils.Trace(MyUtils.toStringSafe(x));
+        //MyUtils.Trace(MyUtils.toStringSafe(x));
         if(x==null) return null;
 
         int cmp = key.compareTo(x.key);
@@ -116,8 +183,8 @@ public class BST<Key extends Comparable<Key>, Value>
         }
         //assert x !=null;
         else if (cmp <0) {
-             MyUtils.Trace("left");
-                x = floor(x.left, key);
+            // MyUtils.Trace("left");
+            x = floor(x.left, key);
 //            Node n;
 //            if (n != null)
 //                x=n;
@@ -126,13 +193,13 @@ public class BST<Key extends Comparable<Key>, Value>
         }
         else if (cmp >0) {
             Node n;
-            MyUtils.Trace("right");
+            //MyUtils.Trace("right");
             n = floor(x.right, key);
             if ( n!= null)
                 x=n;
             else
                 ;//do not update x, nothing found
-                //return x;
+            //return x;
         }
         return x;
     }
@@ -141,6 +208,7 @@ public class BST<Key extends Comparable<Key>, Value>
     public static void main(String[] args){
         //test_isEmpty();
         // test_Contains();
+        test_min();
         test_getPut();
         test_Floor();
     }
@@ -217,5 +285,25 @@ public class BST<Key extends Comparable<Key>, Value>
         MyUtils.AssertEqual(actual, expected, "!Test04");
 
 
+    }
+
+    private static void  test_min() {
+        System.out.println("*test_min");
+        String key, expected,actual;
+        BST<String, Integer> l;
+        key="a";
+        l= new BST<String, Integer>();
+        expected="a";
+        l.put(key, 1);
+        actual=l.min();;
+        MyUtils.AssertEqual(actual, expected, "!Test01");
+
+
+        key="c";
+        //l= new BST<String, Integer>();
+        l.put(key, 1);
+        expected="a";
+        actual=l.min();
+        MyUtils.AssertEqual(actual, expected, "!Test02");
     }
 }
