2dc0a58eb4462ff4ba0f061479cdf381ee52b85e
mdapri <maurizio.dapri@gmail.com>
2015-03-28 21:49:29 +0100

    added the delete methods and counts

--- src/CC.java
+++ src/CC.java
@@ -60,8 +60,8 @@ public class CC {
     public static void main(String[] args){
         System.out.println("START");
         //test_Constructor();
-        test_Connected();
-
+        //test_Connected();
+        test_Quiz();
         System.out.println("END");
 
     }
@@ -95,6 +95,60 @@ public class CC {
         MyUtils.Assert(cc.id(7) ==1, "!Test04");
         MyUtils.Assert(cc.id(9) ==2, "!Test05");
         MyUtils.Assert(cc.id(12) ==2, "!Test06");
+
+    }
+
+    private static void test_Quiz(){
+        System.out.println("*test_Connected");
+        int size=0;
+        Graph g;
+        //replicate the graph  the slide
+        g = new Graph(10);
+        //  A B C D E F G H I J
+        //  0 1 2 3 4 5 6 7 8 9
+        /*
+        A:  B F G  // 0: 1 5 6
+        B:  A H G  // 1: 0 7 6
+        C:  I D    // 2: 8 3
+        D:  I C    // 3: 8 2
+        E:  J      // 4: 9
+        F:  G A    // 5: 6 0
+        G:  F B A H // 6: 5 1 0 7
+        H:  B G     // 7: 1 6
+        I:  D C     // 8: 3 2
+        J:  E       // 9: 4
+         */
+        //A:  B F G
+        g.addEdge(0,1);        g.addEdge(0,5);        g.addEdge(0,6);
+        // 1: 0 7 6
+        g.addEdge(1,7);g.addEdge(1,6);
+        // 2: 8 3
+        g.addEdge(2,8);g.addEdge(2,3);
+        // 3: 8 2
+        g.addEdge(3,8);
+        // 4: 9
+        g.addEdge(4,9);
+        // 5: 6 0
+        g.addEdge(5,6);
+        // 6: 5 1 0 7
+        g.addEdge(6,7);
+
+
+
+        CC cc = new CC(g);
+        System.out.println(cc.toString());
+        /*
+        A 0; 0;
+        B 1; 0;
+        C 2; 1;
+        D 3; 1;
+        E 4; 2;
+        F 5; 0;
+        G 6; 0;
+        H 7; 0;
+        I 8; 1;
+        J 9; 2;
+         */
 
     }
 }
--- src/Digraph.java
+++ src/Digraph.java
@@ -1,16 +1,17 @@
-/**
- * Undirected Graph via adjacency matrix representation
- * Princeton 2, Week 1
- */
 import common.MyUtils;
 
-public class Graph {
+/**
+ * Created by maurizio.dapri on 3/28/2015.
+ */
+public class Digraph {
 
     private int _vertex;
     private  int _edges;
     private  Bag<Integer>[] adj;
 
-    public Graph(int V) {
+
+
+    public Digraph(int V) {
         assert V>=0;
         _vertex=V;
         _edges=0;
@@ -22,19 +23,18 @@ public class Graph {
     }
 
     /**
-    // connects 2 vertices. Parallel edges and self loops allowed
-    */
+     // connects 2 vertices. Parallel edges and self loops allowed
+     */
     public void  addEdge(int v, int w) {
         assert v>=0 && v<_vertex;
         assert w>=0 && w<_vertex;
 
         adj[v].add(w);
-        adj[w].add(v);
         _edges++;
     }
     /**
-    // the adjacency list of the vertex
-    */
+     // the adjacency list of the vertex
+     */
     public Iterable<Integer> adj(int v){
         assert v>=0 && v<_vertex;
         return adj[v];
@@ -56,14 +56,13 @@ public class Graph {
         return _edges;
     }
 
+
     public String toString(){
         StringBuilder b = new StringBuilder();
         for (int v = 0; v < _vertex; v++){
             for (int w : adj(v)) {
-                if(v<w) {
-                    b.append(v + "-" + w);
-                    b.append("\n");
-                }
+                b.append(v + "->" + w);
+                b.append("\n");
             }
         }
         return b.toString();
@@ -85,38 +84,34 @@ public class Graph {
     private static void test_Constructor(){
         System.out.println("*test_Constructor");
         int size=0;
-        Graph g = new Graph(1);
+        Digraph g = new Digraph(1);
 
-        MyUtils.Assert(g.V()==1, "!Test01");
+        MyUtils.Assert(g.V() == 1, "!Test01");
         MyUtils.Assert(g.E()==0, "!Test02");
         //add
-        g = new Graph(2);
+        g = new Digraph(2);
         MyUtils.Assert(g.V() == 2, "!Test03");
         MyUtils.Assert(g.E()==0, "!Test04");
-
-        System.out.println(g.toString());
     }
-
-
     private static void test_add(){
         System.out.println("*test_add");
         int size=0;
-        Graph g = new Graph(1);
+        Digraph g = new Digraph(1);
         g.addEdge(0,0);
         MyUtils.Assert(g.V()==1, "!Test01");
         MyUtils.Assert(g.E()==1, "!Test02");
 
-        g = new Graph(2);
+        g = new Digraph(2);
         g.addEdge(0,1);
         MyUtils.Assert(g.E() == 1, "!Test03");
         MyUtils.Assert(g.adj[0].size()==1, "!Test04");
-        MyUtils.Assert(g.adj[1].size()==1, "!Test05");
+        MyUtils.Assert(g.adj[1].size()==0, "!Test05");
         // add the reverse and check the edge have been added
 
         g.addEdge(1,0);
         MyUtils.Assert(g.E() == 2, "!Test06");
-        MyUtils.Assert(g.adj[0].size()==2, "!Test07");
-        MyUtils.Assert(g.adj[1].size()==2, "!Test08");
+        MyUtils.Assert(g.adj[0].size()==1, "!Test07");
+        MyUtils.Assert(g.adj[1].size()==1, "!Test08");
 
         System.out.println(g.toString());
 
